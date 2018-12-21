ddaceaf06e85f2a70c0e99bd2dda09dfe2ad0d64
nunmino <nun555nun@gmail.com>
2018-12-21 11:21:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projectsc/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.projectsc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView t = findViewById(R.id.textView);
+                t.setText(FirebaseInstanceId.getInstance().getToken());
+                Log.d("Token = ",""+FirebaseInstanceId.getInstance().getToken());
+            }
+        });
+    }
+}
